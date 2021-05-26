67982bd71af5ab038dc7c48d2c2ada7ae8c4e844
Henry T. Garcia <henrytgarcia@uol.com.br>
2021-05-26 15:06:04 -0300

    Project created

---
+++ backend/src/main/java/com/henrygarcia/cap01_CRUD/Cap01CrudApplication.java
@@ -0,0 +1,13 @@
+package com.henrygarcia.cap01_CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cap01CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cap01CrudApplication.class, args);
+	}
+
+}
