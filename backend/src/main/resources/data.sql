c7c49b0b4aeaa29ed2ce576296bfa1cc1996b619
Henry T. Garcia <henrytgarcia@uol.com.br>
2021-05-31 20:28:19 -0300

    Database seeding

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_client (name, cpf, income, children)
+ VALUES ('Henry Garcia', '497.355.963.01', 9000 , 3);
+ INSERT INTO tb_client (name, cpf, income, children)
+ VALUES ('Helena Hazar', '026.321.951.07', 15000 , 0);
+ INSERT INTO tb_client (name, cpf, income, children)
+ VALUES ('Raphael Olivas', '864.501.764.03', 3500 , 1);
