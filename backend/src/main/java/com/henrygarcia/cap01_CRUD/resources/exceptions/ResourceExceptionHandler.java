8dc36d41936be86ec9462b7637e4879068baa827
Henry T. Garcia <henrytgarcia@uol.com.br>
2021-06-01 15:23:55 -0300

    Method update client

--- backend/src/main/java/com/henrygarcia/cap01_CRUD/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/henrygarcia/cap01_CRUD/resources/exceptions/ResourceExceptionHandler.java
@@ -9,13 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.henrygarcia.cap01_CRUD.services.exceptions.EntityNotFoundException;
+import com.henrygarcia.cap01_CRUD.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException en, HttpServletRequest request){
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException en, HttpServletRequest request){
 		StandardError error = new StandardError();
 		error.setTimestamp(Instant.now());
 		error.setStatus(HttpStatus.NOT_FOUND.value()); //codigo 404
