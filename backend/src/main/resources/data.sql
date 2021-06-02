f2f113f520dc4f1e45e64fbc89c980fb52e2ae93
Henry T. Garcia <henrytgarcia@uol.com.br>
2021-06-01 23:11:42 -0300

    Auditing data and Sedding database

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,6 +1,22 @@
-INSERT INTO tb_client (name, cpf, income, children)
- VALUES ('Henry Garcia', '497.355.963.01', 9000 , 3);
-INSERT INTO tb_client (name, cpf, income, children)
- VALUES ('Helena Hazar', '026.321.951.07', 15000 , 0);
-INSERT INTO tb_client (name, cpf, income, children)
- VALUES ('Raphael Olivas', '864.501.764.03', 3500 , 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At)
+ VALUES ('Henry Garcia', '497.355.963.01', 9000 , TIMESTAMP WITH TIME ZONE '1978-07-12T09:01:00Z', 3, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At)
+ VALUES ('Helena Hazar', '026.321.951.07', 15000 , TIMESTAMP WITH TIME ZONE '1982-09-16T17:52:00Z', 0, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At)
+ VALUES ('Raphael Olivas', '864.501.764.03', 3500 , TIMESTAMP WITH TIME ZONE '2004-12-25T22:36:00Z', 1, NOW());
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At)
+ VALUES ('Matheus Fernandes', '625.891.963.x', 300 , TIMESTAMP WITH TIME ZONE '1993-05-23T09:01:00Z', 0, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At)
+ VALUES ('Ricardo B. Neves', '126.321.951.07', 10000 , TIMESTAMP WITH TIME ZONE '1986-10-16T17:52:00Z', 4, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At)
+ VALUES ('Maria Fernanda', '963.571.764.25', 5500 , TIMESTAMP WITH TIME ZONE '2000-06-12T07:30:00Z', 3, NOW());
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At)
+ VALUES ('Renan o Franco', '845.485.021.01', 8550 , TIMESTAMP WITH TIME ZONE '1964-03-05T09:01:00Z', 5, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At)
+ VALUES ('Luffy D. Monkey', '852.021.337.06', 12000 , TIMESTAMP WITH TIME ZONE '1975-11-23T17:52:00Z', 3, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At)
+ VALUES ('Caio Bravo', '747.581.764.05', 2500 , TIMESTAMP WITH TIME ZONE '2003-01-06T22:36:00Z', 1, NOW());
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At)
+ VALUES ('Marcia Cavalcante', '558.891.301.07', 3800 , TIMESTAMP WITH TIME ZONE '1993-05-23T09:01:00Z', 0, NOW());
+
+ 
